4f56df83563ba30f5c1b3ff13a30875e67555389
KKrzych09 <kamil.krzych@poczta.fm>
2021-07-08 17:04:37 +0200

    Moonshot --formatting main view

---
+++ Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Kamil Krzych on 07/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    var body: some View {
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("Detail view")) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+                    
+                    VStack(alignment: .leading) {
+                        Text(mission.DisplayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
+            }
+            .navigationBarTitle("Moonshot")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
