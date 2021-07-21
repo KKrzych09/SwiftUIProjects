f7cd03f90242424f25c3d0aa95c8291a54e13f36
KKrzych09 <kamil.krzych@poczta.fm>
2021-07-21 21:02:17 +0200

    Having fun with drawing

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Kamil Krzych on 18/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
