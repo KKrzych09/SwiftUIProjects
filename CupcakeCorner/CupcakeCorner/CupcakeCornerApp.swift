12b2b46a0b7c834404382848ccfeb6a8907db7a4
KKrzych09 <kamil.krzych@poczta.fm>
2021-07-29 15:52:05 +0200

    CC --basic layout

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Kamil Krzych on 29/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
