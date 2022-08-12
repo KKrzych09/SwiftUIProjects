5641164e592623d65d93e95860fc98b8e0b2a0b6
KKrzych09 <kamil.krzych@poczta.fm>
2022-08-12 11:58:20 +0200

    CoreData entities relationships

---
+++ CoreDataProject/Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Kamil Krzych on 12/08/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Kamil Krzych on 12/08/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
