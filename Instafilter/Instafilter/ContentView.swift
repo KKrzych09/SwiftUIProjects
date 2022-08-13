3bfcf04714bc54ebaad08f713a808f3d922defbd
KKrzych09 <kamil.krzych@poczta.fm>
2022-08-13 10:12:56 +0200

    CoreImage twirl effect

---
+++ Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Kamil Krzych on 13/08/2022.
+//
+
+import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+struct ContentView: View {
+    
+    @State private var image: Image?
+    
+    var body: some View {
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "Example") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+        
+        currentFilter.radius = 1000
+        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
+        
+        // get a CIImage from out filter or exit if that fails
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        // attempt to get a CGImage from our CIImage
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            // convert that to a UIImage
+            let uiImage = UIImage(cgImage: cgimg)
+            
+            // and convert that to SwiftUI image
+            image = Image(uiImage: uiImage)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Kamil Krzych on 13/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
