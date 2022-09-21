18eb570380b601d1d13698aa5d9d5ba7a1f08adf
KKrzych09 <kamil.krzych@poczta.fm>
2022-09-21 12:38:21 +0200

    Basic local notifications setup

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Kamil Krzych on 20/09/2022.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct ContentView: View {
+    
+    @State private var backgroundColor = Color.red
+    
+    var body: some View {
+        VStack {
+            Button("Request permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+                
+                // show this notification five seconds from now
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                // choose a random identifier
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                // add my notification request
+                UNUserNotificationCenter.current().add(request)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
