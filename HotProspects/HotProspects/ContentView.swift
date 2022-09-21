19d8c28d25b0e2099c85ec9e67d4e59d5a837c53
KKrzych09 <kamil.krzych@poczta.fm>
2022-09-21 13:50:23 +0200

    Dependency example

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -6,40 +6,31 @@
 //
 
 import SwiftUI
-import UserNotifications
+import SamplePackage
 
 struct ContentView: View {
     
-    @State private var backgroundColor = Color.red
+    let possibleNumbers = Array(1...60)
+    
+    var results: String {
+        // Select 7 random numbers from possibleNumbers with a method from SamplePackage called random()
+        // which accepts an int and will return up to that number random elements from sequence in random order.
+        // Method sorted() at the end, let us arrange numbers from smallest to largest.
+        let selected = possibleNumbers.random(7).sorted()
+        
+        // Then I need to convert that array of ints (selected) into strings using map() method that lets me convert
+        // an array of one type into an array of another type by applying a function to each element.
+        // In this case, one must initialize a new string from each int, so I can use String.init as the function I want
+        // to call.
+        let strings = selected.map(String.init)
+        
+        // Right now, array contains seven random numbers from my range, so last I want to do is to join them all
+        // together with commas between by using joined method.
+        return strings.joined(separator: ", ")
+    }
     
     var body: some View {
-        VStack {
-            Button("Request permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-                    if success {
-                        print("All set!")
-                    } else if let error = error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            
-            Button("Schedule Notification") {
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the cat"
-                content.subtitle = "It looks hungry"
-                content.sound = UNNotificationSound.default
-                
-                // show this notification five seconds from now
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                // choose a random identifier
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                // add my notification request
-                UNUserNotificationCenter.current().add(request)
-            }
-        }
+        Text(results)
     }
 }
 
